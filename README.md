5a99e721a01210f1a2460682047363bdde34b2ec
Skyler Layne <skylerclayne@gmail.com>
2016-01-20 17:41:58 -0500

    initial commit and addition of assignment 1

---
+++ README.md
@@ -0,0 +1,10 @@
+# AI and Logic Programming Assignments
+
+Here be the assignments for AI.
+
+## Get the Assignment
+
+``` bash
+rsync cse23170@red.cse.yorku.ca:/cs/course/3401/assignment1 .
+```
+
---
+++ assignment1/assignment1.sol
@@ -0,0 +1,10 @@
+EECS 3401 
+ASSIGNMENT 1
+DEADLINE: February 10, 2016
+Skyler Layne (cse23170, 212166906)
+
+
+                  KNOWLEDGE REPRESENTATION and CPL
+
+**PROBLEM I. SOLUTION**: 
+p_off & ~p_opened & p_key -> 
