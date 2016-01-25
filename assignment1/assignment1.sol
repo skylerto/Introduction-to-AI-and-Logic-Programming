70cbb3100a6cca44ac7f8c707ca34d7fb8c9f18b
Skyler Layne <skylerclayne@gmail.com>
2016-01-24 21:56:37 -0500

    added ruby script to fetch assignments

--- assignment1/assignment1.sol
+++ assignment1/assignment1.sol
@@ -1,10 +1,113 @@
 EECS 3401 
-ASSIGNMENT 1
+ASSIGNMENT 1 SOLUTIONS
 DEADLINE: February 10, 2016
 Skyler Layne (cse23170, 212166906)
 
 
-                  KNOWLEDGE REPRESENTATION and CPL
+I.                KNOWLEDGE REPRESENTATION and CPL
 
-**PROBLEM I. SOLUTION**: 
-p_off & ~p_opened & p_key -> 
+PROBLEM 1. 
+
+A1. TURN LOCK ON
+  ENGLISH: (lock off) & (key touched lock) -> (lock on)
+  PROP_LOGIC: p_off & p_t -> ~p_off
+  CNF: ~p_off v ~p_t v ~ p_off 
+
+A2. PULL_DOOR
+  ENGLISH: (door can be opened) & (AN pulls handle)
+  PROP_LOGIC: p_can & p_h
+
+A3. RELEASE LOCK
+  ENGLISH: (key not touching lock) -> (lock off)
+  PROP_LOGIC:
+  CNF: 
+
+A4. TOUCH_LOCK
+  ENGLISH: (has key) -> (key touches)  
+  PROP_LOGIC: p_key -> p_t
+  CNF: 
+
+C. if lock is off, the door is closed, and AN has a key, then he door can be
+opened.
+
+  PROP_LOGIC: p_off & ~p_opened & p_key -> p_can
+  CNF: 
+
+PROBLEM 2:
+(i) -> (ii)
+  -- no tva h, h(KB)={1}
+  --
+
+(ii) -> (iii)
+  -- for all A, KB |- A
+  -- assume A = p & ~p
+  -- therefore KB |- p & ~p
+
+(iii) -> (i)
+  -- no tva h, h(KB)={1}
+
+
+II.                  RESOLUTION RULE: PROPOSITIONAL CASE
+
+PROBLEM 3:
+(a) {-s, p, q, s v -p v -q},
+  C1. ~s
+  C2. p
+  C3. q
+  C4. s v ~p v ~q
+  C5. ~p v ~q, (resolve C1, C4 on s)
+  C6. ~q, (resolve, C2, C5 on p)
+  C7.  ⃞ , (resolve C3, C6 on q)
+  -- therefore INCONSISTENT
+
+(b) {-q v -s v -p, s, s v q, p v s, p v q, p v -q, q v -p},
+  C1. -q v -s v -p
+  C2. s
+  C3. s v q
+  C4. p v s
+  C5. p v q
+  C6. p v -q
+  C7. q v -p
+  C8. ~q v ~p, (resolve C1,C2 on s)
+  C9. p, (resolve C5,C7 on p)
+  C10. ~p, (resolve C7,C8 on q)
+  C11.  ⃞ , (resolve C9,C10 on p)
+  -- therefore INCONSISTENT
+
+(c) {-p v -q v -s, p v -t, -t, q v -t v -p},
+  C1. -p v -q v -s
+  C2. p v -t
+  C3. -t
+  C4. q v -t v -p
+
+  T.V.A
+  s=f -> C1=t
+  t=f -> C2=t, C4=t
+  p,q=t,f
+  -- therefore CONSISTENT
+
+(d) {-p v -s, t v -p, q v -s, q v t, -q v -t, p v s, t v -s, q v -p}
+  C1. -p v -s
+  C2. t v -p
+  C3. q v -s
+  C4. q v t
+  C5. -q v -t
+  C6. p v s
+  C7. t v -s
+  C8. q v -p
+  C9. ~p v p, (resolve C1,C6 on s)
+  C10.
+
+PROBLEM 4: use PROBLEM 1 to prove KB |- C, where KB={A1,A2,A3}
+  KB |- C
+  KB \/ {~C}
+  
+PROBLEM 5:
+
+PROBLEM 6:
+
+III.                  RESOLUTION RULE: PREDICATE LOGIC CASE
+
+PROBLEM 7. tba
+PROBLEM 8. tba
+PROBLEM 9. tba
