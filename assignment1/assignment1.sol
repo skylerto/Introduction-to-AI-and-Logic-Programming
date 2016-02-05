e88f0bde822c15d2717e12fc28bae05443318e83
Skyler Layne <skylerclayne@gmail.com>
2016-02-04 19:06:02 -0500

    Minor Modifications to Assignment1 and My solutions
    This commit is a catch up on the assignment, some things have changed.
    
    Assignment 1 has been changed a little, thi commit reflects some of those
    changes.
    
    my solutions include some updates in the CNF from problem 1. I have also removed
    problem 9 to reflect the professors notes.

--- assignment1/assignment1.sol
+++ assignment1/assignment1.sol
@@ -1,4 +1,4 @@
-EECS 3401 
+EECS 3401
 ASSIGNMENT 1 SOLUTIONS
 DEADLINE: February 10, 2016
 Skyler Layne (cse23170, 212166906)
@@ -6,32 +6,33 @@ Skyler Layne (cse23170, 212166906)
 
 I.                KNOWLEDGE REPRESENTATION and CPL
 
-PROBLEM 1. 
+PROBLEM 1.
 
 A1. TURN LOCK ON
   ENGLISH: (lock off) & (key touched lock) -> (lock on)
   PROP_LOGIC: p_off & p_t -> ~p_off
-  CNF: ~p_off v ~p_t v ~ p_off 
+  CNF: ~p_off v ~p_t v ~ p_off
 
 A2. PULL_DOOR
   ENGLISH: (door can be opened) & (AN pulls handle)
   PROP_LOGIC: p_can & p_h
+  CNF: p_can & p_h
 
 A3. RELEASE LOCK
   ENGLISH: (key not touching lock) -> (lock off)
-  PROP_LOGIC:
-  CNF: 
+  PROP_LOGIC: ~p_t ->p _off
+  CNF:
 
 A4. TOUCH_LOCK
-  ENGLISH: (has key) -> (key touches)  
+  ENGLISH: (has key) -> (key touches)
   PROP_LOGIC: p_key -> p_t
-  CNF: 
+  CNF: ~p_key v p_t
 
 C. if lock is off, the door is closed, and AN has a key, then he door can be
 opened.
 
   PROP_LOGIC: p_off & ~p_opened & p_key -> p_can
-  CNF: 
+  CNF:
 
 PROBLEM 2:
 (i) -> (ii)
@@ -79,11 +80,10 @@ PROBLEM 3:
   C2. p v -t
   C3. -t
   C4. q v -t v -p
-
-  T.V.A
-  s=f -> C1=t
-  t=f -> C2=t, C4=t
-  p,q=t,f
+  -- T.V.A
+  -- s=f -> C1=t
+  -- t=f -> C2=t, C4=t
+  -- p,q=t,f
   -- therefore CONSISTENT
 
 (d) {-p v -s, t v -p, q v -s, q v t, -q v -t, p v s, t v -s, q v -p}
@@ -98,11 +98,33 @@ PROBLEM 3:
   C9. ~p v p, (resolve C1,C6 on s)
   C10.
 
-PROBLEM 4: use PROBLEM 1 to prove KB |- C, where KB={A1,A2,A3}
-  KB |- C
-  KB \/ {~C}
-  
+PROBLEM 4: pt1 - use PROBLEM 1 to prove KB |- C, where KB={A1,A2,A3}
+           pt2 - Will the answer be different if A4 is added to KB?
+
+  pt1 - use PROBLEM 1 to prove KB |- C, where KB={A1,A2,A3}
+    KB |- C
+    KB \/ {~C}
+
+  pt2 - Will the answer be different if A4 is added to KB?
+
+
 PROBLEM 5:
+
+k: bus is operating normally
+r: bus is recoverably failed
+u: bus is unrecoverably failed
+a: bus is active
+n: bus is inactive
+i: no command input to bus
+o: no command output from the bus
+
+A1. n v (i & a) -> o
+A2. (~k & n) v ~(~u v r)
+A3. k -> a
+A4. r -> ~u
+A5. a -> n
+
+Q. ~k -> (a & ~(r v u))
 
 PROBLEM 6:
 
@@ -110,4 +132,3 @@ III.                  RESOLUTION RULE: PREDICATE LOGIC CASE
 
 PROBLEM 7. tba
 PROBLEM 8. tba
-PROBLEM 9. tba
