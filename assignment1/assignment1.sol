0f537f36804c7eaa84bc4380cf6e284a59ab1658
Skyler Layne <skylerclayne@gmail.com>
2016-02-04 19:20:17 -0500

    Formatting and Problem Setup
    Added 'SOLUTION' before writing the solutions
    
    Setup the definitions of problems which were not previously defined.

--- assignment1/assignment1.sol
+++ assignment1/assignment1.sol
@@ -7,105 +7,110 @@ Skyler Layne (cse23170, 212166906)
 I.                KNOWLEDGE REPRESENTATION and CPL
 
 PROBLEM 1.
+SOLUTION
 
-A1. TURN LOCK ON
-  ENGLISH: (lock off) & (key touched lock) -> (lock on)
-  PROP_LOGIC: p_off & p_t -> ~p_off
-  CNF: ~p_off v ~p_t v ~ p_off
+  A1. TURN LOCK ON
+    ENGLISH: (lock off) & (key touched lock) -> (lock on)
+    PROP_LOGIC: p_off & p_t -> ~p_off
+    CNF: ~p_off v ~p_t v ~ p_off
 
-A2. PULL_DOOR
-  ENGLISH: (door can be opened) & (AN pulls handle)
-  PROP_LOGIC: p_can & p_h
-  CNF: p_can & p_h
+  A2. PULL_DOOR
+    ENGLISH: (door can be opened) & (AN pulls handle)
+    PROP_LOGIC: p_can & p_h
+    CNF: p_can & p_h
 
-A3. RELEASE LOCK
-  ENGLISH: (key not touching lock) -> (lock off)
-  PROP_LOGIC: ~p_t ->p _off
-  CNF:
+  A3. RELEASE LOCK
+    ENGLISH: (key not touching lock) -> (lock off)
+    PROP_LOGIC: ~p_t ->p _off
+    CNF:
 
-A4. TOUCH_LOCK
-  ENGLISH: (has key) -> (key touches)
-  PROP_LOGIC: p_key -> p_t
-  CNF: ~p_key v p_t
+  A4. TOUCH_LOCK
+    ENGLISH: (has key) -> (key touches)
+    PROP_LOGIC: p_key -> p_t
+    CNF: ~p_key v p_t
 
-C. if lock is off, the door is closed, and AN has a key, then he door can be
-opened.
+  C. if lock is off, the door is closed, and AN has a key, then he door can be
+  opened.
 
-  PROP_LOGIC: p_off & ~p_opened & p_key -> p_can
-  CNF:
+    PROP_LOGIC: p_off & ~p_opened & p_key -> p_can
+    CNF:
 
 PROBLEM 2:
-(i) -> (ii)
-  -- no tva h, h(KB)={1}
-  --
+SOLUTION
+  (i) -> (ii)
+    -- no tva h, h(KB)={1}
+    --
 
-(ii) -> (iii)
-  -- for all A, KB |- A
-  -- assume A = p & ~p
-  -- therefore KB |- p & ~p
+  (ii) -> (iii)
+    -- for all A, KB |- A
+    -- assume A = p & ~p
+    -- therefore KB |- p & ~p
 
-(iii) -> (i)
-  -- no tva h, h(KB)={1}
+  (iii) -> (i)
+    -- no tva h, h(KB)={1}
 
 
 II.                  RESOLUTION RULE: PROPOSITIONAL CASE
 
 PROBLEM 3:
-(a) {-s, p, q, s v -p v -q},
-  C1. ~s
-  C2. p
-  C3. q
-  C4. s v ~p v ~q
-  C5. ~p v ~q, (resolve C1, C4 on s)
-  C6. ~q, (resolve, C2, C5 on p)
-  C7.  ⃞ , (resolve C3, C6 on q)
-  -- therefore INCONSISTENT
 
-(b) {-q v -s v -p, s, s v q, p v s, p v q, p v -q, q v -p},
-  C1. -q v -s v -p
-  C2. s
-  C3. s v q
-  C4. p v s
-  C5. p v q
-  C6. p v -q
-  C7. q v -p
-  C8. ~q v ~p, (resolve C1,C2 on s)
-  C9. p, (resolve C5,C7 on p)
-  C10. ~p, (resolve C7,C8 on q)
-  C11.  ⃞ , (resolve C9,C10 on p)
-  -- therefore INCONSISTENT
+SOLUTION
+  (a) {-s, p, q, s v -p v -q},
+    C1. ~s
+    C2. p
+    C3. q
+    C4. s v ~p v ~q
+    C5. ~p v ~q, (resolve C1, C4 on s)
+    C6. ~q, (resolve, C2, C5 on p)
+    C7.  ⃞ , (resolve C3, C6 on q)
+    -- therefore INCONSISTENT
 
-(c) {-p v -q v -s, p v -t, -t, q v -t v -p},
-  C1. -p v -q v -s
-  C2. p v -t
-  C3. -t
-  C4. q v -t v -p
-  -- T.V.A
-  -- s=f -> C1=t
-  -- t=f -> C2=t, C4=t
-  -- p,q=t,f
-  -- therefore CONSISTENT
+  (b) {-q v -s v -p, s, s v q, p v s, p v q, p v -q, q v -p},
+    C1. -q v -s v -p
+    C2. s
+    C3. s v q
+    C4. p v s
+    C5. p v q
+    C6. p v -q
+    C7. q v -p
+    C8. ~q v ~p, (resolve C1,C2 on s)
+    C9. p, (resolve C5,C7 on p)
+    C10. ~p, (resolve C7,C8 on q)
+    C11.  ⃞ , (resolve C9,C10 on p)
+    -- therefore INCONSISTENT
 
-(d) {-p v -s, t v -p, q v -s, q v t, -q v -t, p v s, t v -s, q v -p}
-  C1. -p v -s
-  C2. t v -p
-  C3. q v -s
-  C4. q v t
-  C5. -q v -t
-  C6. p v s
-  C7. t v -s
-  C8. q v -p
-  C9. ~p v p, (resolve C1,C6 on s)
-  C10.
+  (c) {-p v -q v -s, p v -t, -t, q v -t v -p},
+    C1. -p v -q v -s
+    C2. p v -t
+    C3. -t
+    C4. q v -t v -p
+    -- T.V.A
+    -- s=f -> C1=t
+    -- t=f -> C2=t, C4=t
+    -- p,q=t,f
+    -- therefore CONSISTENT
+
+  (d) {-p v -s, t v -p, q v -s, q v t, -q v -t, p v s, t v -s, q v -p}
+    C1. -p v -s
+    C2. t v -p
+    C3. q v -s
+    C4. q v t
+    C5. -q v -t
+    C6. p v s
+    C7. t v -s
+    C8. q v -p
+    C9. ~p v p, (resolve C1,C6 on s)
+    C10.
 
 PROBLEM 4: pt1 - use PROBLEM 1 to prove KB |- C, where KB={A1,A2,A3}
-           pt2 - Will the answer be different if A4 is added to KB?
+           pt2 - Will the SOLUTION be different if A4 is added to KB?
 
+SOLUTION
   pt1 - use PROBLEM 1 to prove KB |- C, where KB={A1,A2,A3}
     KB |- C
     KB \/ {~C}
 
-  pt2 - Will the answer be different if A4 is added to KB?
+  pt2 - Will the SOLUTION be different if A4 is added to KB?
 
 
 PROBLEM 5:
@@ -118,17 +123,42 @@ n: bus is inactive
 i: no command input to bus
 o: no command output from the bus
 
-A1. n v (i & a) -> o
-A2. (~k & n) v ~(~u v r)
-A3. k -> a
-A4. r -> ~u
-A5. a -> n
+SOLUTION
+  A1. n v (i & a) -> o
+  A2. (~k & n) v ~(~u v r)
+  A3. k -> a
+  A4. r -> ~u
+  A5. a -> n
 
-Q. ~k -> (a & ~(r v u))
+  Q. ~k -> (a & ~(r v u))
 
 PROBLEM 6:
+  (T) Let S be a set of clauses and let C be a tautological clause in S. Then, S is
+      consistent iff S-{C} is consistent.
+
+  Prove T.
+
+  To prove T we must prove the S is consistent with or without C, i.e. that S is consistent regardless of C.
+
+
+
+
 
 III.                  RESOLUTION RULE: PREDICATE LOGIC CASE
 
-PROBLEM 7. tba
-PROBLEM 8. tba
+PROBLEM 7.
+SOLUTION
+  (a) {p(f(Y),W,g(Z)), p(U,U,V)}
+  --
+
+  (b) {q(X,h(Z),h(X)), q(g(a,Y),h(b),h(Y))}
+  --
+
+  (c) {p(a,X,f(g(Y))), p(Z,h(Z,W),f(W))}
+  --
+
+PROBLEM 8.
+SOLUTION
+  KB2 = {q(X,f(Y,a)):- ; p(X):- ; r(X,g(T)):- q(Z,T)},
+
+  and query r(Z,T) v p(T).
